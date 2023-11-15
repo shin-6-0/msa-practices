b17b19bd2411743a982dfd1fd38421ceaff157a9
shin-6-0 <syk6715@naver.com>
2023-11-15 13:34:44 +0900

    discovery client - mysite08 #1

--- clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/EmaillistApplication.java
+++ clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/EmaillistApplication.java
@@ -2,10 +2,21 @@ package com.poscodx.emaillist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmaillistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmaillistApplication.class, args);
+	}
+	
+	
+	//Resttemplate 사용시 LB 필수
+	@LoadBalanced
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
--- clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
+++ clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/controller/ApiController.java
@@ -1,46 +1,57 @@
 package com.poscodx.emaillist.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import com.poscodx.emaillist.dto.JsonResult;
-import com.poscodx.emaillist.repository.EmaillistRepository;
 import com.poscodx.emaillist.vo.EmaillistVo;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@SuppressWarnings("unchecked")
+@RequestMapping("/api/emaillist")
 public class ApiController {
-	@Autowired
-	private EmaillistRepository emaillistRepository;
-	
-	@GetMapping("/api")
-	public ResponseEntity<JsonResult> read(
-			@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
-		
-		log.info("Request[GET /api]:"+ keyword);
-		
-		return ResponseEntity
-				.status(HttpStatus.OK)
-				.body(JsonResult.success(emaillistRepository.findAll(keyword)));
+
+	private RestTemplate restTemplate;
+
+	public ApiController(@LoadBalanced RestTemplate restTemplate) {
+		//Authwired 대신 생성자를 만들어서 때려넣는 방식을 사용!
+		this.restTemplate=restTemplate;
 	}
 	
-	@PostMapping("/api")
-	public ResponseEntity<JsonResult> create(@RequestBody EmaillistVo vo) {
-		log.info("Request[POST /api]:" + vo);
+	
+	@GetMapping
+	public ResponseEntity<?> read(
+			@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
+		log.info("Request [GET/api] : "+ keyword);
 		
-		emaillistRepository.insert(vo);
+		Map<String,Object> response = restTemplate.getForObject("http://service-emailist/?kw="+keyword, Map.class);
 		
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(JsonResult.success(vo));
+				.body(response);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> create(@RequestBody EmaillistVo vo) {
+		log.info("Request [POST/api] : " + vo);
+		
+		Map<String,Object> response = restTemplate.postForObject("http://service-emailist/",vo,Map.class);	
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(response);
 	}
 	
 }
